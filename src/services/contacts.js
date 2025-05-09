2bf2d1a944c63e103552e902329df0403536921d
Dmytro <d.salyakin@gmail.com>
2025-05-10 02:11:01 +0300

    Implement GET/contacts/:contactId route

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,3 +4,8 @@ export const getAllContacts = async () => {
   const contacts = await Contact.find();
   return contacts;
 };
+
+export const getContactById = async (contactId) => {
+  const contact = await Contact.findById(contactId);
+  return contact;
+};

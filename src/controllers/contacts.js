08f0aed99356a8d29f059598d58e5a3bf3fa685c
Dmytro <d.salyakin@gmail.com>
2025-05-19 00:42:27 +0300

    Add error handling with http-errors middleware

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,3 +1,5 @@
+import createHttpError from 'http-errors';
+
 import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
@@ -15,10 +17,7 @@ export const getContactByIdController = async (req, res, next) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(200).json({
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 
 import contactsRouter from './routers/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -27,18 +29,9 @@ export function setupServer() {
     res.json({ message: 'Hello, Phonebook!' });
   });
 
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, (error) => {
     if (error) {

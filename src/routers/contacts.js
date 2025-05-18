1539092c1714977350db0f7e0f3364e58aa7aa51
Dmytro <d.salyakin@gmail.com>
2025-05-18 21:56:34 +0300

    Move contact controllers to controllers directory

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,34 +1,13 @@
 import { Router } from 'express';
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {
+  getContactByIdController,
+  getContactsController,
+} from '../controllers/contacts.js';
 
 const router = Router();
 
-router.get('/contacts', async (req, res) => {
-  const contacts = await getAllContacts();
+router.get('/contacts', getContactsController);
 
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
-});
-
-router.get('/contacts/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-
-  if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
-    return;
-  }
-
-  res.status(200).json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
-  });
-});
+router.get('/contacts/:contactId', getContactByIdController);
 
 export default router;

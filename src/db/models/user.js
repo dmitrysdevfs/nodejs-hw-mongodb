39f2c569a3cc8ad6f4c28b9a0797b485a1724f7a
Dmytro <d.salyakin@gmail.com>
2025-06-01 17:39:12 +0300

    Add 2 models for users and sessions collections

---
+++ src/db/models/user.js
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+userSchema.method.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model('users', userSchema);
